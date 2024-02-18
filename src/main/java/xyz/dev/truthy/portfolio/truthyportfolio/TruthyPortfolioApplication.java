6199830823fb65ef5840994161df9f3dee788a16
quest <steamnilsoo@gmail.com>
2024-02-18 20:01:12 +0100

    Initial commit

---
+++ src/main/java/xyz/dev/truthy/portfolio/truthyportfolio/TruthyPortfolioApplication.java
@@ -0,0 +1,13 @@
+package xyz.dev.truthy.portfolio.truthyportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruthyPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruthyPortfolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/dev/truthy/portfolio/truthyportfolio/TruthyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.dev.truthy.portfolio.truthyportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruthyPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
